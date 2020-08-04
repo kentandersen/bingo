e134c5b232c313daa418c1184569958267e8754c
Kent Andersen <kent.andersen@bekk.no>
2020-08-04 13:24:03 +0200

    tweaked pixelate

--- src/pixelate.js
+++ src/pixelate.js
@@ -6,22 +6,28 @@ values.forEach(({ img: imgSrc }) => {
     img.src = imgSrc
 })
 
+function setSmoothing(ctx, smoothing) {
+    ctx.msImageSmoothingEnabled = smoothing;
+    ctx.mozImageSmoothingEnabled = smoothing;
+    ctx.webkitImageSmoothingEnabled = smoothing;
+    ctx.imageSmoothingEnabled = smoothing;
+}
+
 export default function createPixelation(canvas) {
     const ctx = canvas.getContext('2d');
     
-    ctx.mozImageSmoothingEnabled = false;
-    ctx.webkitImageSmoothingEnabled = false;
-    ctx.imageSmoothingEnabled = false;
-    
     return (imgSrc) => {
         const img = new Image();
+        setSmoothing(ctx, false);
 
-        const setValue = (v = 0) => {
+
+        let size = 8;
+        const setValue = (v = size) => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
             const size = v * 0.01;
             const w = canvas.width * size;
             const h = canvas.height * size;
             
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.drawImage(img, 0, 0, w, h);
             ctx.drawImage(canvas, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
         }
@@ -30,10 +36,9 @@ export default function createPixelation(canvas) {
         img.onload = () => {
             setValue();
             
-            let size = 8;
             intervalId = setInterval(() => {
-                setValue(size++);
-            }, 700);
+                setValue(++size);
+            }, 250);
         }
         
         img.src = imgSrc;
@@ -41,6 +46,9 @@ export default function createPixelation(canvas) {
 
         return () => {
             clearInterval(intervalId)
+            setSmoothing(ctx, true);
+            
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
         }
     }
